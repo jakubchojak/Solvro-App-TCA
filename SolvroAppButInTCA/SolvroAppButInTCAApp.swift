a24fb07a289b74fc3c5a3575c05c7d0298692b64
Jakub Chojak <abukkajohc@gmail.com>
2023-05-07 12:13:59 +0200

    Initial Commit

---
+++ SolvroAppButInTCA/SolvroAppButInTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolvroAppButInTCAApp.swift
+//  SolvroAppButInTCA
+//
+//  Created by Jakub Chojak on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SolvroAppButInTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
